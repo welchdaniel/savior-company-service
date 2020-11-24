3fc731f075cb2346c91060d6f7d0aa4123009634
welchdaniel <welch.da@husky.neu.edu>
2020-11-23 21:28:46 -0500

    rename main application

--- src/main/java/com/savior/companydatabaseservice/Application.java
+++ src/main/java/com/savior/companydatabaseservice/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CompanyDatabaseServiceApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CompanyDatabaseServiceApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
